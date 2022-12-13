9db40ee8cba9f10cf40324b8342ad9bd002d4b22
Andy <andrew.eliscu@gmail.com>
2022-12-13 14:32:02 -0600

    Add views and CSS

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+import monsters from './monsterData.json' assert {type: 'json'};
+
+//router
 app.get('/', function(request, response) {
-	response.render('home', {pageName: "Home Page"})
+	response.render('home')
+})
+
+app.get('/monsters', function(request, response) {
+	response.render('monsters', {data: monsters})
+})
+
+app.get('/detail', function(request, response) {
+	response.render('detail', {data: monsters})
+})
+
+//if no route matches use use()
+app.use(function(request, response){
+	response.status(404).render('404', {query: request.url})
 })
 
 
