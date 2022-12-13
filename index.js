36b5bde06cc0be434a0f5ca688b276870306a9c3
Andy <andrew.eliscu@gmail.com>
2022-12-13 13:28:29 -0600

    add express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-index.js
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', function(request, response) {
+	response.render('home', {pageName: "Home Page"})
+})
+
+
+// Start app
+app.listen(1982, function(){
+	console.log("server started at http://localhost:1982/")
+});
